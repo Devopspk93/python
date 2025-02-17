74017270d770d09bd43290945b11c3ad2830df99
Devopspk93 <devopspk93@gmail.com>
2025-02-17 23:26:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, this website is coming soon!W!!!W'
+    return 'Hello, this website is coming soon!W!ty!!W'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
